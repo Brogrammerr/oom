3977705df89c71110a3473d0b1d5f7d253c2d94a
Kranthi <ic16b041@technikum.wien-at>
2017-02-23 13:29:54 +0100

    Task2

---
+++ tasks/Task2/Task2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 0;
+            try
+            {
+                switch (a)
+                {
+                    case 0:
+                        Console.WriteLine("a is 0");
+
+                    case 5:
+                        Console.WriteLine("a is 5");
+                        break;
+                    default:
+                        Console.WriteLine("a is something else");
+                        break;
+                }
+            }
+            catch (Exception )
+            {
+                Console.WriteLine("Something happend");
+            }
+        }
+
+    }
+}
