4d42fe638385f8f9ff61a7a9affc8451607ad6a7
Kranthi <ic16b041@technikum.wien-at>
2017-03-07 17:50:27 +0100

    Task2

--- tasks/Task2/Task2/Program.cs
+++ tasks/Task2/Task2/Program.cs
@@ -14,14 +14,25 @@ namespace Task2
         private double guthaben = 100;
 
 
+       
+
         public double Guthaben
         {
             get
             {
-                return guthaben;
+                return guthaben ;
             }
 
+            set {
+
+                if (value < 0)
+                    throw new Exception("Kein Guthaben mehr");
+
+                guthaben = value;
+
+            }
         }
+      
 
 
 
@@ -37,25 +48,38 @@ namespace Task2
 
         public static void Main(string[] args)
         {
-            var Vapiano = new[]
+            var Vapn = new[]
             {
              new Vapiano("Pasta","Pomodoro", 10.30),
              new Vapiano("Pizza", "Salami", 6.70),
              new Vapiano("Bar", "Wein", -45),
             };
 
-            foreach (var b in Vapiano)
+            foreach (var b in Vapn)
             {
                 Console.WriteLine("{0} {1} {2} Guthaben : {3}", b.station, b.gericht, b.preis, b.guthaben);
+                
 
             }
 
-            var p_name = Vapiano.Select(x => x.gericht).OrderBy(x => x);
+            var p_name = Vapn.Select(x => x.gericht).OrderBy(x => x);
 
             Console.WriteLine();
             Console.WriteLine("Gekaufte Gerichte");
             foreach (var x in p_name) Console.WriteLine(x);
-            
+
+            var p_preis= Vapn.Select(x => x.preis).OrderBy(x => x);
+
+          
+            Console.WriteLine();
+
+
+
+            foreach (var x in p_preis)
+            {
+
+                Console.WriteLine(x);
+            }
 
 
 
