07dc9660b9acf45cbe383f41e48b8cf82f2ae731
Kranthi <ic16b041@technikum.wien-at>
2017-03-07 17:22:16 +0100

    Task2

--- tasks/Task2/Task2/Program.cs
+++ tasks/Task2/Task2/Program.cs
@@ -11,11 +11,24 @@ namespace Task2
         private string station;
         private string gericht;
         private double preis;
+        private double guthaben = 100;
+
+
+        public double Guthaben
+        {
+            get
+            {
+                return guthaben;
+            }
+
+        }
+
+
 
         public Vapiano(String Station, String GerichtName, double Gerichtpreis)
         {
 
-            if ((Station == null) || (GerichtName == null)) throw new Exception("Kannst kein Gericht ohne Name erstellen"); 
+            if ((Station == null) || (GerichtName == null)) throw new Exception("Kannst kein Gericht ohne Name erstellen");
             station = Station;
             gericht = GerichtName;
             preis = Gerichtpreis;
@@ -28,19 +41,29 @@ namespace Task2
             {
              new Vapiano("Pasta","Pomodoro", 10.30),
              new Vapiano("Pizza", "Salami", 6.70),
+             new Vapiano("Bar", "Wein", -45),
             };
 
             foreach (var b in Vapiano)
             {
-                Console.WriteLine("{0} {1} {2}", b.station, b.gericht, b.preis);
+                Console.WriteLine("{0} {1} {2} Guthaben : {3}", b.station, b.gericht, b.preis, b.guthaben);
+
             }
+
+            var p_name = Vapiano.Select(x => x.gericht).OrderBy(x => x);
+
+            Console.WriteLine();
+            Console.WriteLine("Gekaufte Gerichte");
+            foreach (var x in p_name) Console.WriteLine(x);
+            
+
+
+
+
+
+
+
+
         }
-
-        
- 
-
-       
-
-
     }
 }
