bcc6aac09046100122bc726a3a0a71295dc38f60
Kranthi <ic16b041@technikum.wien-at>
2017-03-21 17:10:02 +0100

    Task4

---
+++ tasks/Task4/Task4/Employee.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace Task4
+{
+    class Employee:IE_Person
+    {
+        private string  _profession;
+
+
+        
+        public Employee(string name, int age, string profession):this(name,age)
+        {
+            if (string.IsNullOrWhiteSpace(profession)) throw new ArgumentException("You cant be empty", nameof(profession));
+            
+            _profession = profession;
+        }
+
+
+        [JsonConstructor]
+        public Employee(string name, int age)
+        {
+            if (string.IsNullOrWhiteSpace(name)) throw new ArgumentException("You cant be empty", nameof(name));
+            Name = name;
+
+            if (age == 0) throw new ArgumentException("You cant be 0 years old.", nameof(age));
+            Age = age;
+
+
+        }
+
+
+        public string Name { get; }
+       public int Age { get; }
+
+
+        public int Salary(int age)
+        {
+
+
+            if (age > 30)
+            {
+
+                return 2500;
+
+            }
+
+            else
+            {
+                return 1300;
+            }
+        }
+
+    }
+}
---
+++ tasks/Task4/Task4/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using Newtonsoft.Json;
+
+namespace Task4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var people = new IE_Person[]
+            {
+                new Student("Max",23,"Informatics",2),
+                new Student("Filz",27,"Software_engineering",4),
+                new Student("Kranthi",23,"Information and communication Systems", 2),
+
+                new Employee("Hans", 45, "Network_Admin"),
+                new Employee("Michi", 19, "Junior_Programmer"),
+
+
+            };
+
+            var settings = new JsonSerializerSettings() {TypeNameHandling = TypeNameHandling.Auto, Formatting = Formatting.Indented };
+            var jsonp = JsonConvert.SerializeObject(people,settings);
+            var cwd = Directory.GetCurrentDirectory();
+            var filename = Path.Combine(cwd, "people.json");
+            File.WriteAllText(filename, jsonp);
+
+
+            var djson = File.ReadAllText(filename);
+            var deser = JsonConvert.DeserializeObject<IE_Person[]>(djson,settings);
+            
+
+
+            foreach (var x in deser)
+            {
+                Console.WriteLine("Name:{0} , age: {1}",x.Name , x.Age);
+
+
+                
+            }
+
+
+
+
+
+
+        }
+
+    }
+}
